7f9757043c251589b0e11d53196f0eef2054c303
NineOhm <ohmthanwa1205@gmail.com>
2024-07-26 19:15:20 +0700

    change target.mind & add srcret code

--- src/app/api/reveal-code/route.ts
+++ src/app/api/reveal-code/route.ts
@@ -5,6 +5,7 @@ export async function POST(request: Request) {
 
   const hiddenCodes: {[key: number]: string} = {
     0: 'SECRET_CODE_FITWHEY_01',
+    1: 'SECRET_CODE_FITWHEY_02',
   }
 
   return NextResponse.json({ code: hiddenCodes[targetIndex] || 'No code found' })
